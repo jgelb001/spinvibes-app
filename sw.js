0f217adbd065fab7150531b0ec5a199de8370166
Jeremy Gelbaum <jeremy.gelbaum@gmail.com>
2026-06-13 07:37:59 -0700

    Update app 2026-06-13 07:37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spinvibes-app-v9';
+const CACHE = 'spinvibes-app-v10';
 const SHELL = ['/', '/index.html', '/confirm.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
