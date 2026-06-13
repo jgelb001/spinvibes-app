5443c963170dcb311a2dd9913c911c5cecc7663a
Jeremy Gelbaum <jeremy.gelbaum@gmail.com>
2026-06-13 04:39:16 +0000

    Editable scorecard-scan review + handicap backfill on past rounds 2026-06-13 04:39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spinvibes-app-v3';
+const CACHE = 'spinvibes-app-v4';
 const SHELL = ['/', '/index.html', '/confirm.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
