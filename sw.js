19fdbd558391782f8b5a7af991dda668b2e286e9
Jeremy Gelbaum <jeremy.gelbaum@gmail.com>
2026-06-13 04:22:18 +0000

    Coach's Plan markdown, handicap index, scorecard OCR, caddie nav-overlap fix, Short Game back button 2026-06-13 04:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spinvibes-app-v2';
+const CACHE = 'spinvibes-app-v3';
 const SHELL = ['/', '/index.html', '/confirm.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
